1dfc09772e9e5fffd2f399a36d8b58a719226cc7
b6220500652 <narisara.kaa@ku.th>
2021-03-18 17:26:57 +0700

    100%

--- 6220500652_6_2.c
+++ 6220500652_6_2.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int x,y=0,n,i;
+    int x=0,y=0,n,i;
     scanf("%d",&n);
     char a[n],b[n];
     scanf("%s",a);
     scanf("%s",b);
     for(i=0;i<n;i++)
     {
-        x=0;
 		if(b[i]==a[i])
         {
             x++;
